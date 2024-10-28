ec0369bca6c57ffdc861bb08ce61e879e6379515
BlazejKonsek <bl.konsek@gmail.com>
2024-10-28 09:55:31 +0100

    Add files via upload

---
+++ php_02_ochrona_dostepu/app/calc_view.php
@@ -0,0 +1,47 @@
+<?php require_once dirname(__FILE__) .'/../config.php';
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+?>
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+<head>
+<meta charset="utf-8" />
+<title>Kalkulator Kredytu</title>
+</head>
+<body>
+
+<div style="width:90%; margin: 2em auto;">
+    <a href="<?php print(_APP_URL); ?>/app/security/logout.php" class="pure-button pure-button-active">Wyloguj</a>
+</div>
+
+<form action="<?php print(_APP_URL);?>/app/calc.php" method="post">
+    <label for="id_kwota">Kwota: </label>
+    <input id="id_kwota" type="text" name="kwota" value="<?php if (isset($kwota)) print($kwota); ?>" /><br />
+    <label for="id_lata">Lat: </label>
+    <input id="id_lata" type="text" name="lata" value="<?php if (isset($lata)) print($lata); ?>" /><br />
+    <label for="id_opr">Oprocentowanie: </label>
+    <input id="id_opr" type="text" name="oprocentowanie" value="<?php if (isset($oprocentowanie)) print($oprocentowanie); ?>" /><br />
+    <input type="submit" value="Oblicz" />
+</form>    
+
+<?php
+if (isset($messages)) {
+    if (count($messages) > 0) {
+        echo '<ol style="margin: 20px; padding: 10px 10px 10px 30px; background-color: #f88; width:300px;">';
+        foreach ($messages as $msg) {
+            echo '<li>'.$msg.'</li>';
+        }
+        echo '</ol>';
+    }
+}
+?>
+
+<?php if (isset($result)) { ?>
+<div style="margin: 20px; padding: 10px; background-color: #ff0; width:300px;">
+    <?php echo 'Wynik: '.$result; ?>
+</div>
+<?php } ?>
+
+</body>
+</html>
